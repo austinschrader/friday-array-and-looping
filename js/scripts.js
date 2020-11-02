a1f4b02f48b95b611636c3b6b91302d90d10222d
Austin Schrader <austin.jschrader@gmail.com>
2020-11-02 07:55:08 -0800

    Separate backend and frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+function numberTo(userInput) {
+	for (let i = 0; i < userInput; i++) {
+		if (i.includes('1')) {
+			console.log('This is a 1 digit');
+		}
+		$('.userResults').append('</br> Hello');
+	}
+}
+
 $(document).ready(function () {
-	$('').submit(function () {
-		let pizzaInput = $('#pizzaOne').val();
-		let pizzaOutput = $('#pizaTwo').val();
+	$('#formOne').submit(function () {
+		event.preventDefault();
+		let userInput = parseInt($('#userInput').val());
+		$('.userResults').append(userInput);
 	});
 });
