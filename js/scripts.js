69793921c8f2ed6ecac331e4bdf310056ff33994
Austin Schrader <austin.jschrader@gmail.com>
2020-11-08 19:23:42 -0800

    Remove unnecessary code that duplicated the if/else statements already present

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,12 @@ function numberTo(userInput) {
 	let userResults = Array.from(Array(parseInt(userInput)).keys());
 
 	for (let i = 0; i < userResults.length; i++) {
-		if (userResults[i].toString() === '13') {
+		if (userResults[i].toString().includes(3)) {
 			userResults[i] = 'Wont you be my neighbor? ';
-		} else if (userResults[i].toString() === '21') {
-			userResults[i] = 'Boop ';
-		} else if (userResults[i].toString() === '32') {
-			userResults[i] = 'Wont you be my neighbor? ';
-		} else if (userResults[i].toString().includes(1)) {
-			userResults[i] = 'Beep! ';
 		} else if (userResults[i].toString().includes(2)) {
 			userResults[i] = 'Boop! ';
-		} else if (userResults[i].toString().includes(3)) {
-			userResults[i] = 'Wont you be my neighbor? ';
+		} else if (userResults[i].toString().includes(1)) {
+			userResults[i] = 'Beep! ';
 		}
 	}
 	return userResults;
